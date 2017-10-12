e6b34a90f0426dd0189aa5cb470529a284b65cda
Ivan <dreamteamstankin@gmail.com>
2017-10-12 13:20:41 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,72 @@
+var TelegramBot = require('node-telegram-bot-api');
+var fs = require('fs');
+var token = '447746908:AAFOv0sCAjOo8PwSBrvf8vdkGOnssFgkzd0';
+var bot = new TelegramBot(token, {polling: true});
+
+var notes = [];
+
+var options = {
+      reply_markup: JSON.stringify({
+        inline_keyboard: [
+          [{ text: '/напомни %заметка% в hh:mm', callback_data: '/напомни' }],
+          [{ text: '/заметки', callback_data: '/заметки' }],
+          [{ text: '/удалить', callback_data: '/удалить' }],
+        ]
+      })
+    };
+
+bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
+      var userId = msg.from.id;
+      var text = match[1];
+      var time = match[2];
+    
+      notes.push( { 'uid':userId, 'time':time, 'text':text } );
+      
+      bot.sendMessage(userId, 'Отлично! Жди, я за тобой приду');
+      console.log(userId, text, time);
+    });
+
+
+
+bot.onText(/\/заметки/, function (msg, match) {
+  var userId = msg.from.id;
+
+  console.log(userId);
+
+  if (notes.length == 0) {
+        bot.sendMessage(userId, 'У вас нет заметок');
+  }
+for (var i = 0; i < notes.length; i++){
+    bot.sendMessage(userId, 'Вы должны "' + notes[i]['text'] + ' в ' + notes[i]['time'] + "\" ");
+}
+  
+});
+
+bot.onText(/\/удалить/, function (msg, match) {
+        var userId = msg.from.id;
+  for (var i = 0; i < notes.length; i++){    
+    notes.splice(i,1);
+
+  };
+  bot.sendMessage(userId, 'Записи удалены');
+});
+
+
+bot.onText(/\/help/, function (msg, match) {
+    bot.sendMessage(msg.chat.id, 'Список команд:', options);
+});
+
+setInterval(function(){
+        for (var i = 0; i < notes.length; i++){
+            var curDate = new Date().getHours() + ':' + new Date().getMinutes();
+                if ( notes[i]['time'] == curDate ) {
+                    bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+                    notes.splice(i,1);
+                }
+            }
+    },1000);
+
+
+
+
+
